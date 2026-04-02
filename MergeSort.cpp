30f1ba869b96b4e9074f3cc71361ba064439cdcc
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-02 16:17:57 +0700

    Membuat fungsi input dasar

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -6,3 +6,11 @@ int arr[20], B[20];
 // n is array input size
 int n;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        
