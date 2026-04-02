14f6cfeab9a18088e6ff13b540e1853943bd1699
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-02 16:27:19 +0700

    Menyallin hasil merge dan menghasilkan output

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -90,3 +90,19 @@ void mergeSort(int low, int high)
     }
 }
 
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n - 1);
+    output();
+}
